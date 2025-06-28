a46bad58e745c19b5c85218bd39147f81385e229
João Vitor Pimentel <joao.pimentel2017@outlook.com>
2025-06-28 14:36:45 -0300

    Back dependencias

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import animeRoutes from './routes/animeRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cacheRoutes from './routes/cacheRoutes.js';
@@ -11,6 +12,7 @@ import { logAuthentication, logActivity } from './middleware/logger.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+<<<<<<< Updated upstream
 // securityMiddleware(app);
 
 app.use(express.json({ limit: '10mb' }));
@@ -19,6 +21,11 @@ app.use(sanitizeInput);
 app.use(logAuthentication);
 app.use(logActivity);
 
+=======
+app.use(cors());
+app.use(express.json());
+app.use('/', animeRoutes);
+>>>>>>> Stashed changes
 app.use('/', authRoutes);
 app.use('/', animeRoutes);
 app.use('/', cacheRoutes);
